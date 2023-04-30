09cfc70c926a34360810cc655505d8603dcc50ef
Alexey Starodubov <124338833+Rasbeek@users.noreply.github.com>
2023-04-30 14:07:43 +0700

    Update Задание 19.3.3.py
    Исправлены ошибки в синтаксисе

--- Задание 19.3.3.py
+++ Задание 19.3.3.py
@@ -23,7 +23,7 @@ res = requests.post(f'https://petstore.swagger.io/v2/pet', headers=headers, json
 
 #Вывод кода твета и данных о питомце запроса
 print(res.status_code)
-print(res.json)
+print(res.json())
 
 #Изменение данных питомца
 headers = {'accept': 'application/json', 'Content-Type': 'application/json'}
@@ -35,14 +35,14 @@ res = requests.put(f'https://petstore.swagger.io/v2/pet', json=data)
 
 #Вывод кода твета и данных о питомце запроса
 print(res.status_code)
-print(res.json)
+print(res.json())
 
 #Удаление данных питомца
 res = requests.delete(f'https://petstore.swagger.io/v2/pet/{9223372036854741000}', headers={'accept':'application/json'})
 
 #Вывод кода твета и данных о питомце запроса
 print(res.status_code)
-print(res.json)
+print(res.json())
 
 #Вывод списка всех доступных питомцев
 status = 'available'
